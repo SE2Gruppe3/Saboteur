24a7cae9c649853fcb1ac89d7c86618130b3c8eb
Bastian Schroeder <bastian.schroeder99@gmail.com>
2026-05-03 15:37:34 +0200

    Add block/unblock and double tool cards (enum, deck, UI mapping). Closes #64

--- app/app/src/main/java/com/aau/saboteur/ui/TunnelCardDrawableMapper.kt
+++ app/app/src/main/java/com/aau/saboteur/ui/TunnelCardDrawableMapper.kt
@@ -5,6 +5,19 @@ import com.aau.saboteur.model.Direction
 import com.aau.saboteur.model.TunnelCard
 
 internal fun TunnelCard.toDrawableName(): String = when (type) {
+    // Map all special tool cards directly to their drawable resource names
+    CardType.CART_RED -> "cart_red"
+    CardType.CART_GREEN -> "cart_green"
+    CardType.LANTERN_RED -> "lantern_red"
+    CardType.LANTERN_GREEN -> "lantern_green"
+    CardType.PICKAXE_RED -> "pickaxe_red"
+    CardType.PICKAXE_GREEN -> "pickaxe_green"
+    CardType.TRACK_RED -> "track_red"
+    CardType.TRACK_GREEN -> "track_green"
+    CardType.DOUBLE_LANTERN_CART -> "double_lantern_cart"
+    CardType.DOUBLE_PICKAXE_CART -> "double_pickaxe_cart"
+    CardType.DOUBLE_PICKAXE_LANTERN -> "double_pickaxe_lantern"
+
     CardType.START -> "start"
     CardType.GOAL -> when (id) {
         "goal_gold" -> "goal_gold"
@@ -33,4 +46,19 @@ internal fun TunnelCard.toContentDescription(): String = when (type) {
     CardType.GOAL -> if (isRevealed) "Revealed goal card" else "Hidden goal card"
     CardType.PATH -> "Path card"
     CardType.DEAD_END -> "Dead end card"
+
+
+    CardType.CART_RED,
+    CardType.CART_GREEN,
+    CardType.LANTERN_RED,
+    CardType.LANTERN_GREEN,
+    CardType.PICKAXE_RED,
+    CardType.PICKAXE_GREEN,
+    CardType.TRACK_RED,
+    CardType.TRACK_GREEN,
+    CardType.DOUBLE_LANTERN_CART,
+    CardType.DOUBLE_PICKAXE_CART,
+    CardType.DOUBLE_PICKAXE_LANTERN
+        -> "Special tool card"
+
 }
