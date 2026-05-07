699c58ea766451e7768b32db64f09325325ad5a1
Anonymoushacker0815 <eelukasee@gmail.com>
2026-05-07 12:59:49 +0200

    Lockfiles to satisfy sonar code analysis

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,3 +8,9 @@ plugins {
     id("org.springframework.boot") version "3.4.3" apply false
     id("io.spring.dependency-management") version "1.1.7" apply false
 }
+
+allprojects {
+    dependencyLocking {
+        lockAllConfigurations()
+    }
+}
